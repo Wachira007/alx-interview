cb7da5d64e288638c4cf3b7714c6f998d3db3636
Wachira007 <dannielmutua00@gmail.com>
2023-03-26 21:33:58 +0300

    changes

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-def pascal_triangle(n):
+"""Pascal Triangle Interview Challenge"""
 
-    """Pascal Challenge"""
-    # Check if n is less than or equal to 0, and return an empty list if so
+
+def pascal_triangle(n):
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
     if n <= 0:
         return []
-    
-    # Create an empty list to hold the Pascal's triangle
-    triangle = [[1]]
-    
-    # Iterate over each row of the triangle (starting from the second row)
-    for i in range(1, n):
-        # Create a new list to hold the current row of the triangle
-        row = [1]
-        
-        # Iterate over each element in the middle of the row
+
+    pascal_triangle = [0] * n
+
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
         for j in range(1, i):
-            # Calculate the sum of the corresponding elements in the previous row,
-            # and add this value to the current row
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        
-        # Add the last element (which is always 1) to the current row
-        row.append(1)
-        
-        # Add the current row to the triangle
-        triangle.append(row)
-    
-    # Return the completed triangle.
-    return triangle
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
